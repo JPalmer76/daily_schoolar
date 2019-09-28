a70e761e4afde5b199a9ac3171dc68eba6cc5e53
Jonny <jpdadof3@gmail.com>
2019-09-28 13:00:40 -0500

    something

--- daily_schoolar/client/src/App.js
+++ daily_schoolar/client/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
 import Admin from "./Components/AdminPage/Admin";
+import LoginPage from "./Components/LoginPage/LogIn";
 
 
 
 function App() {
   return (
    
-    <Admin />
+    <LoginPage />
     
   );
 }
--- daily_schoolar/client/src/Components/AdminPage/Admin.js
+++ daily_schoolar/client/src/Components/AdminPage/Admin.js
@@ -1,52 +1,11 @@
-import React, { Component } from 'react';
-import "./Style.css";
+import React, { Component } from 'react'
 
 export default class Admin extends Component {
   render() {
     return (
-      <container>
+      <div>
         
-       <form className="col s6">
-       <h3 id="adminTitle">Parent/Teacher Login</h3>
-      <div class="row">
-      <div class="col s3"/>
-        <div class="input-field col s6">
-          <input id="email" type="email" class="validate"/>
-          <label for="email">Email</label>
-        </div>
       </div>
-      
-      <div class="row">
-      <div class="col s3"/>
-        <div class="input-field col s6">
-          <input id="password" type="password" class="validate"/>
-          <label for="password">Password</label>
-        </div>
-      </div>
-      <button class="btn waves-effect waves-light" type="submit" name="action">Submit</button>
-  
-  <form className="col s6">
-  <h3 id="adminTitle">Student Login</h3>
-  <div class="row">
-  <div class="col s3"/>
-    <div class="input-field col s6">
-      <input id="email" type="email" class="validate"/>
-      <label for="email">Email</label>
-    </div>
-  </div>
-  <div class="row">
-  <div class="col s3"/>
-    <div class="input-field col s6">
-      <input id="password" type="password" class="validate"/>
-      <label for="password">Password</label>
-    </div>
-  </div>
-  <button class="btn waves-effect waves-light" type="submit" name="action">submit</button>
-</form>
-</form>
-</container>
-        
-      
     )
   }
 }
