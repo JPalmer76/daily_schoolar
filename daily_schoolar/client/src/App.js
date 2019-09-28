7031bf10934fd30cfb1b346454adbd6079727aea
Jonny <jpdadof3@gmail.com>
2019-09-28 13:20:04 -0500

    trying to sort out the node_modules

--- daily_schoolar/client/src/App.js
+++ daily_schoolar/client/src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./App.css";
-import Admin from "./Components/AdminPage/Admin";
+// import Admin from "./Components/AdminPage/Admin";
 import LoginPage from "./Components/LoginPage/LogIn";
+import Nav from "./Components/Navbar/Nav"
 
 
 
 function App() {
   return (
-   
+   <wrapper>
+     <Nav />
     <LoginPage />
+    </wrapper>
     
   );
 }
