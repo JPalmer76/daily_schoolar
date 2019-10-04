6cf51fc4aa16f78be4db9b4445038e60a8859df0
Amy Wolgemuth <awolgemuth@trinisys.loc>
2019-10-03 20:52:05 -0500

    updated a few things in login and admin

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,11 @@
 import React from "react";
 import "./App.css";
-
-
-require('../daily_schoolar/server.js.js')
-
 import Admin from "./Components/AdminPage/Admin";
 import LoginPage from "./Components/LoginPage/LogIn";
 import Nav from "./Components/Navbar/Nav";
 import Footer from "./Components/Footer/Footer";
 import Student from "./Components/StudentPage/Student"
 import Register from "./Components/RegisterPage/Register";
-
-
-
 
 function App() {
   return (
@@ -25,14 +18,12 @@ function App() {
      {/* <LoginPage /> */}
      {/* </div> */}
        <div>
-        {/* <Admin /> */}
+        <Admin />
         {/* <Student /> */}
-        <Register />
+        {/* <Register /> */}
      <Footer />
      </div> 
-     </wrapper>
-   
-    
+     </wrapper>  
 
   );
 }
--- client/src/Components/Navbar/Nav.js
+++ client/src/Components/Navbar/Nav.js
@@ -6,7 +6,7 @@ export default class Nav extends Component {
     return (
         <nav>
     <div class="nav-wrapper">
-      <a href="#" class="brand-logo">Logo</a>
+      <a href="#" class="brand-logo">Daily Schoolar</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a href="sass.html">Sass</a></li>
         <li><a href="badges.html">Components</a></li>
